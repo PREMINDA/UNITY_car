8c76fe3de51ead942843cb1b3b19bbc998ec21dc
PREMINDA <premindabandara0777@gmail.com>
2021-03-13 01:23:22 +0530

    add torque

---
+++ Assets/Script/Drive.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Drive : MonoBehaviour
+{
+    [SerializeField] private WheelCollider WH;
+    public float torque = 200;
+    void Start()
+    {
+        WH = this.GetComponent<WheelCollider>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float VER = Input.GetAxis("Vertical");
+        move(VER);
+    }
+
+    void move(float acceleration)
+    {
+        acceleration = Mathf.Clamp(acceleration, -1, 1);
+        float thrustTorque = acceleration * torque;
+        WH.motorTorque = thrustTorque;
+    }
+}
