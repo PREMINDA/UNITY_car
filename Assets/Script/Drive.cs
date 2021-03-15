f13061daad8722fb4fb9ca8ee56f921aff2ae069
PREMINDA <premindabandara0777@gmail.com>
2021-03-16 01:33:36 +0530

    wheel rotation

--- Assets/Script/Drive.cs
+++ Assets/Script/Drive.cs
@@ -6,6 +6,7 @@ public class Drive : MonoBehaviour
 {
     [SerializeField] private WheelCollider WH;
     public float torque = 200;
+    public float maxAngle = 30f;
     public GameObject Wheel;
     void Start()
     {
@@ -16,14 +17,17 @@ public class Drive : MonoBehaviour
     void Update()
     {
         float VER = Input.GetAxis("Vertical");
-        move(VER);
+        float HOS = Input.GetAxis("Horizontal");
+        move(VER,HOS);
     }
 
-    void move(float acceleration)
+    void move(float acceleration , float ang)
     {
         acceleration = Mathf.Clamp(acceleration, -1, 1);
+        ang = Mathf.Clamp(ang, -1, 1) * maxAngle;
         float thrustTorque = acceleration * torque;
         WH.motorTorque = thrustTorque;
+        WH.steerAngle = ang;
         Quaternion qut;
         Vector3 pos;
         WH.GetWorldPose(out pos,out qut);
